9be57946353f0359f552baee82a5f1e6ee1897fa
nabury <98603626+nabury@users.noreply.github.com>
2022-03-04 14:34:31 +0000

    Add files via upload

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ library(shiny)
 library(shinycssloaders)
 library(shinyvalidate)
 
-# Load default data
+# Load default data (Likiti outbreak)
 defaultData <- read.csv("Likiti_outbreak.csv")
 
 ui <- navbarPage("End of Outbreak Probability",
@@ -20,7 +20,7 @@ ui <- navbarPage("End of Outbreak Probability",
             
             sidebarPanel(
         
-                # Import the outbreak data as a csv file
+                # Import the outbreak data csv file
                 fileInput("outbreak_csv", "Select outbreak data file (.csv format) to upload",
                           multiple = TRUE
                          ,
@@ -28,26 +28,24 @@ ui <- navbarPage("End of Outbreak Probability",
                                      "text/comma-separated-values,text/plain",
                                      ".csv")),
                 
-                # Import the serial interval as a csv file
+                # Import the serial interval csv file
                 fileInput("serial_interval_csv", "Select serial interval file (.csv format) to upload",
                           multiple = TRUE,
                           accept = c("text/csv",
                                      "text/comma-separated-values,text/plain",
                                      ".csv")),
-                # actionButton("button", "Reset to pre-loaded data")
-                # Reset button resets the data but then does not allow new uploaded files to be displayed
                 
+                # Action button to reset to pre-loaded data
+                actionButton("button", "Reset to pre-loaded data"),
+
                 p("See documentation tab for guidance on uploading data")
             ),
             
             mainPanel(
         
-                # Display outbreak data in a table
-                tableOutput('outbreak_tbl'),
+                tableOutput('outbreak_tbl'), # Display outbreak data in a table
                 
-                # Display the serial interval in a table
-                tableOutput('serial_interval_tbl')
-                
+                tableOutput('serial_interval_tbl') # Display the serial interval in a table
             ),
         ),
     ),
@@ -60,20 +58,25 @@ ui <- navbarPage("End of Outbreak Probability",
 
                  p("Inputs for offspring distribution"),
                  
+                 # Input for R
                  numericInput("R", 
                               h3("R"), 
                               value = 2.1,
                               step = 0.05), 
                  
+                 # Input for k (overdispersion parameter)
                  numericInput("k", 
                               h3("k"), 
                               value = 0.18,
                               step = 0.005), 
                  
-                 sliderInput("future_days", h3("Future days"),
+                 # Days to be plotted after last reported case
+                 sliderInput("future_days", h5("Days after last known case"),
                              min = 1, max = 50, value = 25),
                  
-                 actionButton("go", "Plot")
+                 # Button to complete calculations
+                 p("Calculate outbreak probabilities using the selected inputs:"),
+                 actionButton("go", "Click")
                  
              ),
              
@@ -94,11 +97,11 @@ ui <- navbarPage("End of Outbreak Probability",
         sidebarLayout(
             
             sidebarPanel(
-                downloadButton("downloadData", "Download results as .csv")
+                downloadButton("downloadData", "Download results as .csv") # Button to download results as a csv
             ),
             
             mainPanel(
-                tableOutput('results_tbl')
+                tableOutput('results_tbl') # Display results in a table
             ),
         ),
     ),
@@ -108,13 +111,13 @@ ui <- navbarPage("End of Outbreak Probability",
         HTML("<b>Outbreak data file</b>"),
         
         p("A csv file containing outbreak data can be uploaded to the app.
-          The file must consist of three columns with the headings named exactly: ID, Onset_ID, Infector_ID.
+          The file must consist of three columns with the headings named exactly: ID, Onset_day, Infector_ID.
           All columns must contain only numeric, integer data."),
         
         HTML("<ul>
             <li>Column 1: ID must be numbered in integer order i.e. 1,2,3. </li>
-            <li>Column 2: Onset_ID contains the outbreak day when the case began.</li>
-            <li>Infector_ID; Refers to the ID of the individual who infected this case. If this is unknown, it should be set to zero.</li>
+            <li>Column 2: Onset_day contains the outbreak day when the case began i.e. the number of days after the first reported case.</li>
+            <li>Infector_ID: Refers to the ID of the individual who infected this case. If this is unknown, it should be set to zero.</li>
             </ul>"),
         
         HTML("<b>Serial interval file</b>"),
@@ -122,7 +125,7 @@ ui <- navbarPage("End of Outbreak Probability",
         p("A serial interval can also be uploaded. This must contain only one column of data with the heading: Serial_interval.
           This column should sum to 1."),
         
-        HTML("<bLast updated 1st March 2022</b>"),
+        HTML("<b>Last updated 4th March 2022</b>"),
         
         p("Change log"),
         
@@ -134,20 +137,24 @@ ui <- navbarPage("End of Outbreak Probability",
             <li>Bug fixes with uploaded serial intervals - 21/02/22</li>
             <li>Button for when inputs are updated - 01/03/22</li>
             <li>Documentation including guidance on uploading csv files - 01/03/22</li>
+            <li>Ability to reset preloaded data - 04/03/22</li>
             </ul>"),
         
         p("Future updates"),
         
         HTML("<ul>
             <li>Interactive combined plot</li>
-            <li>Ability to reset preloaded data</li>
             </ul>"),
     )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    # Reactively change outbreak data between default and uploaded file
+    observeEvent(input$button, {
+        session$reload()
+    })
+    
+    # Reactively change outbreak data between pre-loaded and uploaded data
     outbreak_data <- reactive({
         
         eventReactive(input$button, {
@@ -184,6 +191,7 @@ server <- function(input, output) {
         }
     })
     
+    # Convert to df with correct column name
     serial_interval_df <- reactive ({
         serial_interval_df <- as.data.frame(serial_interval())
         colnames(serial_interval_df) <- ("Serial_interval")
@@ -198,14 +206,17 @@ server <- function(input, output) {
     
     # Check validity of numeric inputs for R and k
     iv <- InputValidator$new()
-    iv$add_rule("R", compose_rules(sv_gt(0), sv_lte(100)))
-    iv$add_rule("k", sv_gt(0))
+    iv$add_rule("R", compose_rules(sv_gt(0), sv_lte(100))) # 0 < R <= 100
+    iv$add_rule("k", sv_gt(0)) # k > 0
     iv$enable()
     
-    # Process end of outbreak probabilities
+    # Calculate end of outbreak probabilities
     results <- eventReactive (input$go, {
-        req(iv$is_valid())
         
+        # Check input validity
+        req(iv$is_valid()) 
+        
+        # Get input data
         outbreak_data <- outbreak_data()
         w <- serial_interval()
         
@@ -213,9 +224,10 @@ server <- function(input, output) {
         max_t <- starting_t + input$future_days # Total days from first case to end of calculation
         
         # Lengthen serial interval
-        if (length(w) < max_t) {w <- c(w, rep(0, max_t - length(w)))}
-            
-        p_outbreak_over <- rep(NA, (max_t)) # Empty vector to hold probabilities
+        if (length(w) < max_t) {w <- c(w, rep(0, max_t - length(w)))} 
+
+        # Create empty vector to store end of outbreak probabilities
+        p_outbreak_over <- rep(NA, (max_t)) 
         
         # For each day into the future
         for (t in (starting_t+1):(max_t)) {
@@ -243,6 +255,7 @@ server <- function(input, output) {
             p_outbreak_over[t] <- p # Record probability for day t in vector
         }
         
+        # Create data frame of times and outbreak probabilities
         times <- c(1:(starting_t+input$future_days))
         results <- data.frame(times, p_outbreak_over)
         return(results)
@@ -289,11 +302,13 @@ server <- function(input, output) {
         return(plot)
     })
     
+    # Table of end of outbreak probability results
     output$results_tbl <- renderTable({
         results()%>%
         rename(Outbreak_day = times, End_of_outbreak_probability = p_outbreak_over)
     })
     
+    # Allow end of outbreak probabilities to be downloaded as a csv file
     output$downloadData <- downloadHandler(
         filename = "End_of_outbreak.csv",
         content = function(file) {
@@ -302,7 +317,6 @@ server <- function(input, output) {
                       file)
         }
     )
-    
 }
 
 shinyApp(ui = ui, server = server)
