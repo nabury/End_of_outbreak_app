9cdb1ac6f10e23a411266ce17fe518398bb11926
Naomi Bradbury <98603626+nabury@users.noreply.github.com>
2023-05-26 13:37:53 +0100

    Update app.R

--- App/app.R
+++ App/app.R
@@ -13,7 +13,7 @@ EbolaSerialInterval <- read.csv("Ebola_serial_interval.csv") # Load Ebola serial
 NipahData <- read.csv("Nipah_outbreak.csv") # Load Nipah data
 NipahSerialInterval <- read.csv("Nipah_serial_interval.csv") # Load Nipah serial interval 
 
-ui <- navbarPage("End-of-Outbreak Probability",
+ui <- navbarPage("End-of-Outbreak Probability v1.0.0",
                  
 
   tabPanel("Home",
